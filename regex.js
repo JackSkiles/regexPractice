23e70729f580d6df51001d99445a33321ccfb446
Jack Skiles <jackskiles420@gmail.com>
2022-06-24 21:49:59 -0400

    continued practice with regex logic tests

--- regex.js
+++ regex.js
@@ -1,13 +1,25 @@
 let contDiv = document.getElementById("mainDiv").children;
 console.log(contDiv.length)
 
-let text = textHeader.textContent;
-const testText = RegExp(/(h{0}e)|e$/);
-console.log(text)
+// let text = textHeader.textContent;
+const testText = RegExp(/e/);
+// console.log(text)
 let array1;
-array1 = testText.exec(text);
+// array1 = testText.exec(text);
 
-console.log(array1)
+array1 = [];
+
+function checkRegEx()
+{
+    for(let i = 0; i < contDiv.length; i++)
+    {
+        let header = document.getElementById(`regexTest${i}`)
+        console.log(header.textContent);
+        array1.push(testText.exec(header));
+    }
+    return array1;
+}
+console.log(checkRegEx());
 // if (textHeader == "/Welcome to Regex testing/")
 // {
 //     console.log("hello");
